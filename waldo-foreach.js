f468d6a3fb1d89c70f35f2bb3270feacdd151639
Jimmy Chicaiza <jimmy.chicaiza@gmail.com>
2019-05-02 16:32:01 -0400

    Waldo forEachand more

--- waldo-foreach.js
+++ waldo-foreach.js
@@ -22,3 +22,28 @@ function actionWhenFound(indexPosition) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+// a new one with and other callback at the end
+
+
+
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+
+ arr.forEach(function(array, i) {
+   if (array === "Waldo") {
+   found(i)
+    }
+   })
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(result) {
+  console.log("The result is:", result);
+});
+//function actionWhenFound(indexPosition) {
+  //console.log("Found him at index " + indexPosition);
+
+
+
