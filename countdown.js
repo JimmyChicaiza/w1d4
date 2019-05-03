09156fde2569d7ebd64a5f6c06bc8a51e9f00e8a
Jimmy Chicaiza <jimmy.chicaiza@gmail.com>
2019-05-03 12:15:26 -0400

    commit the work countdown

--- countdown.js
+++ countdown.js
@@ -1,30 +1,24 @@
-var countdownGenerator = function (x) {
-    var autoNumber = x;
+var countdownGenerator = function(x) {
+  var autoNumber = x;
+  return function() {
+    if (autoNumber > 0) {
+      console.log("T-minus " + autoNumber + "...");
+      autoNumber--;
+    } else if (autoNumber === 0) {
+      console.log("Blast Off");
+      autoNumber--;
+    } else if (autoNumber < -0) {
+      console.log("Rockets already gone, bub!");
+      autoNumber--;
+    }
+  };
+};
 
-        if (autoNumber > 0){
-            return function(){
-                autoNumber -- 
-                console.log("T-minus " + autoNumber + "...");
-            }
-        } else if (autoNumber = 0){
-            return function(){
-            console.log("Blast Off");
-            }
-        }
-    
-        if (autoNumber < 0){
-            return function(){
-            console.log("Rockets already gone, bub!");
-            }
-        }
-    
-    };
-    
-    var countdown = countdownGenerator(3);
-    
-      countdown(); // T-minus 3...
-      countdown(); // T-minus 2...
-      countdown(); // T-minus 1...
-      countdown(); // Blast Off!
-      countdown(); // Rockets already gone, bub!
-      countdown(); // Rockets already gone, bub!
+var countdown = countdownGenerator(3);
+
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
