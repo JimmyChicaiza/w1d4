b75b9a5c1e81f20bee5bd35f99ae229acaa80bd6
Jimmy Chicaiza <jimmy.chicaiza@gmail.com>
2019-05-02 23:40:21 -0400

    forgot this one but is nothing really

--- test.js
+++ test.js
@@ -1,21 +1,14 @@
-function makeIdGenerator() {
-    var id = 0;
+function makeLoadedDie() {
+    var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+    /* your code here */
   
-    // The following is the closure function
     return function() {
-                                            // This inner function accesses and assigns the value of
-                                            // the variable id, which was defined in the parent function
-      id += 1;
-      return id;
+      /* your code here */
     }
   }
   
-  // makeIdGenerator returns a function which is assigned to
-  // the variable nextId
-  var nextId = makeIdGenerator();
+  var rollLoadedDie = makeLoadedDie();
   
-  console.log(nextId()); // Logs: 1
-  console.log(nextId()); // Logs: 2
-  console.log(nextId());
-  console.log(nextId());
-  console.log(nextId());
+  console.log(rollLoadedDie());  // 5
+  console.log(rollLoadedDie());  // 4
+  console.log(rollLoadedDie());  // 6
